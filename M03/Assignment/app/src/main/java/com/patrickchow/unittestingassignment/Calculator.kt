e2d19b6272698f418450ca9a84f91e03b4d9e147
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-02 21:17:57 -0400

    Realized that the assignment was edited. Redoing it.

--- M03/Assignment/app/src/main/java/com/patrickchow/unittestingassignment/Calculator.kt
+++ M03/Assignment/app/src/main/java/com/patrickchow/unittestingassignment/Calculator.kt
@@ -1,7 +1,24 @@
 package com.patrickchow.unittestingassignment
 
-class Calculator (){
-    fun addSymbol(myString: String): String{
-        return myString
+import kotlin.math.absoluteValue
+
+class Math (){
+    fun sumOfSquares(number1: Long, number2: Long): Long{
+        return ((number1 * number1) + (number2 * number2))
+    }
+
+    fun absoluteDifference(number1: Long, number2: Long): Long{
+        val different = number1 - number2
+        return different.absoluteValue
+    }
+}
+
+class Calculator(val math: Math){
+    fun factorial(number1: Long, number2: Long): Long{
+        return math.sumOfSquares(number1, number2)
+    }
+
+    fun difference(number1: Long, number2: Long): Long{
+        return math.absoluteDifference(number1, number2)
     }
 }
